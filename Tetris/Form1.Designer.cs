a68bfebd07704f6653c348a3602b6e23d6c86a8d
Andrew <malstoun@gmail.com>
2012-10-28 17:22:36 +0400

    Added button for rotate

--- Tetris/Form1.Designer.cs
+++ Tetris/Form1.Designer.cs
@@ -37,6 +37,7 @@
             this.button3 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
             this.myCanvas1 = new Tetris.myCanvas();
             this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
@@ -87,10 +88,10 @@
             // splitContainer1.Panel1
             // 
             this.splitContainer1.Panel1.Controls.Add(this.myCanvas1);
-            this.splitContainer1.Panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer1_Panel1_Paint);
             // 
             // splitContainer1.Panel2
             // 
+            this.splitContainer1.Panel2.Controls.Add(this.button5);
             this.splitContainer1.Panel2.Controls.Add(this.button4);
             this.splitContainer1.Panel2.Controls.Add(this.button3);
             this.splitContainer1.Panel2.Controls.Add(this.button2);
@@ -107,6 +108,7 @@
             this.button4.TabIndex = 3;
             this.button4.Text = "Right";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button3
             // 
@@ -116,6 +118,7 @@
             this.button3.TabIndex = 2;
             this.button3.Text = "Left";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button2
             // 
@@ -134,6 +137,16 @@
             this.button1.TabIndex = 0;
             this.button1.Text = "Up";
             this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(64, 32);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(85, 23);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Rotate";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // myCanvas1
             // 
@@ -179,6 +192,7 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
         private myCanvas myCanvas1;
+        private System.Windows.Forms.Button button5;
     }
 }
 
